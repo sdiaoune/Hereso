8e9d68cd8dba1ed6d43249901dc4d957bb016f1f
sdiaoune <soyadiaoune@gmail.com>
2023-01-27 15:37:48 -0500

    change text for website

--- src/App.js
+++ src/App.js
@@ -31,16 +31,16 @@ function App() {
       <section className='App-section'>
         
         <div className='div-description'>
-        <h2>Luxury Apartment in Bamako, ML</h2>
+        <h2>Luxury Apartment in Niarela, Bamako, ML</h2>
         <br/>
         <p>
-        The Luxury Apartment Complex in Niarela, Bamako, Mali is the epitome of opulent living. Nestled in a prime location, just a mere 5 minutes away from the bustling downtown area, this complex offers residents the perfect blend of urban convenience and serene luxury. Boasting 3 grand and fully furnished apartments, each with 3 sumptuous bedrooms, all featuring state-of-the-art air-conditioning systems, this complex is the epitome of comfort and class.
+        The Luxury Apartment Complex in Niarela (close to Cité du Niger) is the epitome of opulent living. Nestled in a prime location, just a mere 5 minutes away from the bustling downtown area, this complex offers residents the perfect blend of urban convenience and serene luxury. Boasting 3 grand and fully furnished apartments, each with 3 sumptuous bedrooms, all featuring state-of-the-art air-conditioning systems, this complex provides serenity, cleanliness, comfort and class.
         </p>
         <p>
-        The grounds of the complex are nothing short of stunning, featuring a meticulously manicured garden, and a sparkling pool, ideal for unwinding and enjoying the Mali sun. Each apartment is equipped with the latest technology, including satellite television and high-speed fiber internet, as well as an American-style kitchen, perfect for hosting dinner parties. The complex also offers a concierge service, shuttle bus, parking and babysitters, ensuring that all your needs are met with the utmost efficiency and discretion.
+        The grounds of the complex are nothing short of stunning, featuring a meticulously manicured garden, and a sparkling pool, ideal for unwinding and enjoying the Mali sun. Each apartment is equipped with the latest technology, including satellite television and high-speed fiber internet, as well as an American-style kitchen, perfect for hosting dinner parties. The complex also offers a concierge service, shuttle bus, washing & dryer machines, video surveillance cameras and parking, ensuring that all your needs are met with the utmost efficiency and discretion.
         </p>
         <p>
-        Step into the realm of indulgence and experience the ultimate in luxury living at the Niarela Apartment Complex.
+        Step into the realm of indulgence and experience the ultimate in luxury living at the Hereso Apartment Complex.
         </p>
         <div>
           <Button>View Floorplans</Button>
--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -13,13 +13,13 @@ function AboutUs() {
             About Us
           </h1>
           <br/>
-          <p>Welcome to the Niarela Apartment Complex, the epitome of opulent living in Bamako, Mali. Our prime location, just 5 minutes from the bustling downtown area, offers residents the perfect blend of urban convenience and serene luxury.</p>
+          <p>Welcome to the Niarela Apartment Complex, the epitome of opulent living in Niarela, Bamako, Mali. Our prime location, just 5 minutes from the bustling downtown area, offers residents the perfect blend of urban convenience and serene luxury.</p>
 
           <p>Our complex features 3 grand and fully furnished apartments, each with 3 sumptuous bedrooms and state-of-the-art air-conditioning systems for ultimate comfort and class. The grounds of the complex boast a meticulously manicured garden and sparkling pool, perfect for unwinding and enjoying the Mali sun.</p>
 
-          <p>Each apartment is equipped with the latest technology, including satellite television and high-speed fiber internet, as well as an American-style kitchen, perfect for hosting dinner parties. To ensure that all your needs are met with the utmost efficiency and discretion, we offer a concierge service, shuttle bus, parking, and babysitters.</p>
+          <p>Each apartment is equipped with the latest technology, including satellite television and high-speed fiber internet, as well as an American-style kitchen, perfect for hosting dinner parties. To ensure that all your needs are met with the utmost efficiency and discretion, we offer a concierge service, shuttle bus, and parking.</p>
 
-          <p>Step into the realm of indulgence and experience the ultimate in luxury living at the Niarela Apartment Complex. Contact us today to schedule a tour and discover the luxury lifestyle that awaits you.</p>
+          <p>Step into the realm of indulgence and experience the ultimate in luxury living at the Hereso Apartment Complex. Contact us today to schedule a tour and discover the luxury lifestyle that awaits you.</p>
           </div>
         </section>
         <GridLayout/>
